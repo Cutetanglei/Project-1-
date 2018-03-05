61c9e7c8d8e3d8fdc9c3981db18ca6cdb4a8f067
Francis <china.lixintong@gmail.com>
2018-03-05 22:29:58 +0800

    主页数据加载 以及用户登陆后界面变更

--- js/logic/index.js
+++ js/logic/index.js
@@ -3,7 +3,27 @@ AV.init({
     appKey: "uYOq35PscfChdJ4Xv8EoqUQb"
 });
 $(function(){
-			
+    
+    function loadData(){
+
+        var phone_pro = new AV.Query('Product')
+        phone_pro.equalTo("categoryName","Mobile Phones")
+
+        var phone_sku = new AV.Query('Product_SKU');
+        phone_sku.limit(3)
+        // phone_sku.descending('createdAt');
+        phone_sku.include(['product'])
+        phone_sku.matchesQuery("product",phone_pro)
+        phone_sku.find().then(function(res){
+            console.log(res[0])
+            var Mobile_Phones =  template("product",{data:res})
+            $("#home").html(Mobile_Phones)
+        })
+    }
+    
+    loadData();
+    
+
     $("#cusRegisterFrom").on("submit",function(e){
         e.preventDefault();
         var nickName = $("#reg_nickName").val();
@@ -29,9 +49,7 @@ $(function(){
                 email:email,
                 password:pwd
             }).then(function(object) {
-                $('#myModal88').modal('hide');
-                sessionStorage.setItem("Customer_ID",object.id)
-                sessionStorage.setItem("Customer_Name",object.attributes.nickName)
+                login(object.id,object.attributes.nickName)
             })
         });
     })
@@ -50,14 +68,41 @@ $(function(){
         var query = AV.Query.and(q_email,q_pwd)
         query.find().then((res)=>{
             if(res.length > 0){
-                $('#myModal88').modal('hide');
-                sessionStorage.setItem("Customer_ID",res[0].id)
-                sessionStorage.setItem("Customer_Name",res[0].attributes.nickName)
+                login(res[0].id,res[0].attributes.nickName)
             }else{
                 alert("用户名或密码错误");
                 return
             }
         })
     })
+    function checkLogin(){
+        if(sessionStorage.getItem("Customer_Name")){
+            logined(sessionStorage.getItem("Customer_Name"))
+        }
+    };
+    checkLogin();
+
+    function login(id,name){
+        $('#myModal88').modal('hide');
+        sessionStorage.setItem("Customer_ID",id)
+        sessionStorage.setItem("Customer_Name",name)
+        logined(name);
+        
+    }
+
+    function logined(name){
+        $(".w3l_login > a").prepend(name).attr("data-target",null).on("click",function(){
+            if(confirm("退出登陆？")){
+                logout();
+            }
+        }) 
+        $(".w3l_login > a > span").removeClass("glyphicon-user").addClass("glyphicon-remove")
+    }
+
+    function logout(){
+        sessionStorage.removeItem("Customer_ID")
+        sessionStorage.removeItem("Customer_Name")
+        window.location.reload();
+    }
 
 });
